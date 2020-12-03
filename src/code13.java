161d098920f483c2aa300903b93f3dadca69b328
me <1912088450@qq.com>
2020-12-03 17:04:46 +0800

    🐖而已

---
+++ src/code13.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class code13 {
+    public boolean isEven(int num){
+        if(num % 2 == 0)
+            return true;
+        else
+            return false;
+    }
+
+    public void reOrder(int[] array){
+        if(array == null || array.length == 0)
+            return;
+        int begin = 0;
+        int end = array.length - 1;
+        while (begin <= end){
+            while (!isEven(array[begin]))
+                begin ++;
+            while (isEven(array[end]))
+                end --;
+            int temp = array[begin];
+            array[begin] = array[end];
+            array[end] = temp;
+        }
+    }
+
+    public static void main(String[] args){
+        int[] array  ={3, 4, 6, 5, 1, 7, 8, 11, 10};
+        System.out.println(Arrays.toString(array));
+        code13 code13 = new code13();
+        code13.reOrder(array);
+        System.out.println(Arrays.toString(array));
+
+    }
+}
